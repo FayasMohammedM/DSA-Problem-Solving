9bdb3a55e056450c64d11d052715371447fda344
FayasMohammedM <fayasmohammedm.tech@gmail.com>
2025-10-09 08:25:12 +0530

    Create FindPivotIndex.java

---
+++ 01_Arrays_Basic/FindPivotIndex.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int left =0;
+        int total=0;
+
+        for (int i =0 ;i<nums.length;i++){
+            total+=nums[i];
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(left==total-left-nums[i]){
+                return i;
+            }
+            left += nums[i];
+        }
+        return -1;
+    }
+}
+
+what was wrong here
